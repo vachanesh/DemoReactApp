75ab26f382e9803d43887810097eec8ea8d6d96e
vachanesh <vachaneshsai98@gmail.com>
2020-02-11 09:57:49 +0530

    test

---
+++ gg/js/PdfFlipper01/PdfFlipper/scripts/tools.js
@@ -0,0 +1,79 @@
+﻿'use strict';
+
+function flipkey() {
+
+    //Aplicar flip
+    $(window).ready(function () {
+        $('.magazine').turn({
+            display: 'double',
+            acceleration: true,
+            gradients: !$.isTouch,
+            elevation: 50,
+            when: {
+                turned: function (e, page) { }
+            }
+        });
+    });
+
+    //Evento Presionar tecla
+  
+        $(window).bind('keydown', function (e) {
+            if (e.keyCode == 37 )
+                $('.magazine').turn('previous');
+            else if (e.keyCode == 39)
+                $('.magazine').turn('next');
+        });
+
+    }
+
+
+    function previous() {
+
+        $('.magazine').turn('previous');
+
+    }
+
+    function next() {
+
+        $('.magazine').turn('next');
+
+    }
+
+// Renderizar Pag
+function renderPage(num, hoja) {
+    pdfDoc.getPage(num).then(function (page) {
+        var viewport = page.getViewport(scale);
+        hoja.height = viewport.height;
+        hoja.width = viewport.width;
+        var renderContext = {
+            canvasContext: hoja.getContext('2d'),
+            viewport: viewport
+        };
+        page.render(renderContext);
+    });
+}
+
+
+// Pasar parametro por Url 
+function getUrlVars() {
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for (var i = 0; i < hashes.length; i++) {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
+
+
+   
+
+
+
+//Cargando pag
+function demoDisplay() {
+    document.getElementById("carga").style.display = "none";
+
+}
+
